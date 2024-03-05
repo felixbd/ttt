fc11cc49ebf034625a2667429775c0f2b7239a1d
Felix D <fdrees@uni-bremen.de>
2024-03-05 17:12:34 +0100

    SHIT ===> GOLD

--- App.tsx
+++ App.tsx
@@ -1,11 +1,32 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
 
-export default function App() {
+function TaskList(props: { tasks: string[] }) :null {
+  return null;
+}
+
+function TaskForm(props: { onAddTask: (task: string) => void }) :null {
+  return null;
+}
+
+export default function App() :React.JSX.Element {
+  let tasks: string[] = [];
+  let addTask = (task: string) :void => { tasks.push(task) };
+
   return (
     <View style={styles.container}>
+
+
       <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
+
+      // a list of all tracked tasks
+      <TaskList tasks={tasks} />
+
+      // a form to add a new task
+      <TaskForm onAddTask={addTask} />
+
     </View>
   );
 }
